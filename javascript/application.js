f675fa3b21ed5b224b8a38828355b4a563506e42
Enrique Ramirez <nrqrmz@gmail.com>
2024-05-11 11:56:44 -0500

    mustache

--- javascript/application.js
+++ javascript/application.js
@@ -1,17 +1,14 @@
+import Mustache from "mustachejs";
+
 const results = document.getElementById('results')
 const url = "http://www.omdbapi.com/?s=harry potter&apikey=adf1f2d7"
-const template = document.getElementById('movieCardTemplate')
+const template = document.getElementById('movieCardTemplate').innerHTML
 
 fetch(url)
   .then(response => response.json())
   .then((data) => {
     data.Search.forEach((movie) => {
-      const clone = template.content.cloneNode(true)
-      clone.querySelector('img').src = movie.Poster
-      clone.querySelector('h2').innerText = movie.Title
-      clone.querySelector('p').innerText = movie.Year
-      clone.querySelector('a').href = `https://www.imdb.com/title/${movie.imdbID}`
-
-      results.appendChild(clone)
+      const output = Mustache.render(template, movie)
+      results.insertAdjacentHTML('beforeend', output)
     })
   })
