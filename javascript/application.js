62bdffa8671a997190cda29800e95dc91ae9c8cb
Enrique Ramirez <nrqrmz@gmail.com>
2024-05-11 11:42:30 -0500

    template

--- javascript/application.js
+++ javascript/application.js
@@ -1,21 +1,17 @@
 const results = document.getElementById('results')
 const url = "http://www.omdbapi.com/?s=harry potter&apikey=adf1f2d7"
+const template = document.getElementById('movieCardTemplate')
 
 fetch(url)
   .then(response => response.json())
   .then((data) => {
     data.Search.forEach((movie) => {
-      const movieCard = `<div class='col-3'>
-      <div class="card bg-white shadow my-3">
-        <img src="${movie.Poster}" class="card-img-top" alt="${movie.Title} poster">
-        <div class="card-body">
-          <h2 class="card-title">${movie.Title}</h2>
-          <p class="card-text">${movie.Year}</p>
-          <a href="https://www.imdb.com/title/${movie.imdbID}" class="btn btn-primary" target="_blank">Go on IMDB</a>
-        </div>
-      </div>
-    </div>`
+      const clone = template.content.cloneNode(true)
+      clone.querySelector('img').src = movie.Poster
+      clone.querySelector('h2').innerText = movie.Title
+      clone.querySelector('p').innerText = movie.Year
+      clone.querySelector('a').href = `https://www.imdb.com/title/${movie.imdbID}`
 
-    results.insertAdjacentHTML('beforeend', movieCard)
+      results.appendChild(clone)
     })
   })
