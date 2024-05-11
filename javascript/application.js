9d8750df2d9dd35874892b30517feb528ffcd668
Enrique Ramirez <nrqrmz@gmail.com>
2024-05-11 11:26:49 -0500

    javascript

--- javascript/application.js
+++ javascript/application.js
@@ -1,1 +1,21 @@
-console.log("hello from application.js")
+const results = document.getElementById('results')
+const url = "http://www.omdbapi.com/?s=harry potter&apikey=adf1f2d7"
+
+fetch(url)
+  .then(response => response.json())
+  .then((data) => {
+    data.Search.forEach((movie) => {
+      const movieCard = `<div class='col-3'>
+      <div class="card bg-white shadow my-3">
+        <img src="${movie.Poster}" class="card-img-top" alt="${movie.Title} poster">
+        <div class="card-body">
+          <h2 class="card-title">${movie.Title}</h2>
+          <p class="card-text">${movie.Year}</p>
+          <a href="https://www.imdb.com/title/${movie.imdbID}" class="btn btn-primary" target="_blank">Go on IMDB</a>
+        </div>
+      </div>
+    </div>`
+
+    results.insertAdjacentHTML('beforeend', movieCard)
+    })
+  })
